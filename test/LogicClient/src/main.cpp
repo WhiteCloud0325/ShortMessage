3fa223b68f8634e1f38bfed2d0400c72345e35e3
WhiteCloud0325 <721715576@qq.com>
2018-12-09 19:26:10 +0800

    add test program

---
+++ test/LogicClient/src/main.cpp
@@ -0,0 +1,32 @@
+#include "im.pb.h"
+#include "LogicInterface.h"
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include <string>
+using namespace apache;
+using namespace apache::thrift;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::protocol;
+
+int main() {
+    UserRegisterRequest user_register_request;
+    user_register_request.set_number("15620623833");
+    user_register_request.set_password("123456");
+    user_register_request.set_nickname("wei");
+    user_register_request.set_ip("162.105.85.45");
+    std::string request;
+    user_register_request.SerializeToString(&request);
+    std::string server_ip = "162.105.85.118";
+    int port = 9090;
+    boost::shared_ptr<TSocket> client_socket(new TSocket(server_ip, port));
+    boost::shared_ptr<TTransport> client_transport(new TBufferedTransport(client_socket));
+    boost::shared_ptr<TProtocol> client_protocol(new TBinaryProtocol(client_transport));
+    boost::shared_ptr<im::LogicInterfaceClient> client(new im::LogicInterfaceClient(client_protocol));
+    im::Request client_request;
+    client_request.__set_type(im::REGISTER_REQUEST);
+    client_reqeust.__set_content(request);
+    client_transport->open();
+    client->SendMessage(client_request);
+    client_transport->close();
+    return 0;
+}
