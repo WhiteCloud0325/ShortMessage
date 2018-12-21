ccedecd86a1a1793c980c61ee517e7464b991492
WhiteCloud0325 <721715576@qq.com>
2018-12-21 20:37:27 +0800

    fix: test server thrift

--- test/LogicServer/src/LogicInterfaceHandler.cpp
+++ test/LogicServer/src/LogicInterfaceHandler.cpp
@@ -1,18 +1,46 @@
 #include "LogicInterfaceHandler.h"
-#include "proto/im.pb.h"
+#include "logic/protocol.h"
 #include <string>
 
 using namespace im;
 
-void LogicInterfaceHandler::AckMessage(const Response&message) {
-    if (!message.__isset.content) {
-        return;
-    }
-    MessageItem mi;
-    if (!mi.ParseFromString(message.content)) {
-        return;
-    }
-    std::string content = mi.DebugString();
-    std::cout << content << std::endl;
+LogicInterfaceHandler::LogicInterfaceHandler(Controller* controller):
+    controller_(controller) {
 }
 
+void LogicInterfaceHandler::AccessToLogic(const std::string& request) {
+    if (request.empty()) {
+        return;
+    }
+    ControlHead* control_head = (ControlHead*)request.c_str();
+
+    switch (control_head->type) {
+        case Login:
+            controller_->mobile_manager_.ProcessLogin(control_head);
+            break;
+        case Logout:
+            controller_->mobile_manager_.ProcessLogout(control_head);
+            break; 
+        case MOBILE_MANAGER_REQUEST:
+            controller_->mobile_manager_.ProcessMobileRequest(control_head);
+            break;
+        case SIMPLE_MESSAGE:
+            controller_->message_manager_.ProcessSimpleMessage(control_head);
+            break;
+        case FORWARD_NOACK_MESSAGE:
+            controller_->message_manager_.ProcessForwardNoAckMessage(control_head);
+            break;
+        case BACKWARD_NOACK_MESSAGE:
+            controller_->message_manager_.ProcessBackwardNoAckMessage(control_head);
+            break;
+        case COMPLETE_MESSAGE:
+            controller_->message_manager_.ProcessCompleteMessage(control_head);
+            break;
+        case INQUIRE_MESSAGE_REQUEST:
+            break;
+        case RECEIPTE:
+            break;
+        default:
+            break;
+    }
+}
--- test/LogicServer/src/LogicInterfaceHandler.h
+++ test/LogicServer/src/LogicInterfaceHandler.h
@@ -2,13 +2,16 @@
 #define LogicInterfaceHandler_H_
 
 #include "thrift/LogicInterface.h"
+#include "workmanager/controller.h"
 
 using namespace im;
 class LogicInterfaceHandler: virtual public LogicInterfaceIf {
 public:
-    LogicInterfaceHandler(){}
-    virtual void SendMessage(const Request& message){};
-    virtual void AckMessage(const Response& message);
+    LogicInterfaceHandler(Controller *controller);
+    virtual void AccessToLogic(const std::string& request);
+    virtual void LogicToAccess(const AccessMessage& response){};
+private:
+    Controller *controller_;
 };
 
 
