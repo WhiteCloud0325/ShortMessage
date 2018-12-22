22c04aef5439c9c590d88309c9e20835cf037736
WhiteCloud0325 <721715576@qq.com>
2018-12-22 10:52:07 +0800

    修改ResponseEncode

--- src/logic/coder.cpp
+++ src/logic/coder.cpp
@@ -4,19 +4,19 @@
 #include "logic/protocol.h"
 
 std::string ResponseEncode(const Response &response) {
-    uint8_t buf[1024] = {0};
-    uint8_t* pos = buf;
+    char buf[1024] = {0};
+    char* pos = buf;
     *(uint32_t *)pos = response.to_id;
     pos +=4;
     *(uint32_t *)pos = response.from_id;
     pos +=4;
     *(uint16_t *)pos = response.frame_id;
     pos +=2;
-    *(pos++) = response.type;
-    *(pos++) = response.retain;
-    *(pos++) = response.receipt_type;
-    *(pos++) = response.receipt_indicate;
-    std::string res((char*)buf, pos - buf);
+    *(uint8_t*)pos++ = response.type;
+    *(uint8_t*)pos++ = response.retain;
+    *(uint8_t*)pos++ = response.receipt_type;
+    *(uint8_t*)pos++ = response.receipt_indicate;
+    std::string res(buf, (pos - buf) + 1);
     return res;
 }
 
