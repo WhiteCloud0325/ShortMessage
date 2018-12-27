e900fbe25b23e3f63dce5f76e4de5a6e3a83a757
WhiteCloud0325 <721715576@qq.com>
2018-12-27 11:11:26 +0800

    test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include "commons/tcp_socket.h"
 #include "commons/comm_socket.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
     char buf[1024] = {0};
@@ -40,5 +43,17 @@ int main() {
     tcp_socket.Create();
     tcp_socket.Connect("162.105.85.118", 12000);
     tcp_socket.SendPacket(buf, pos - buf);
+    char recv_buf[1024] = {0};
+    int recv_len = 0;
+    tcp_socket.RecvPacket(recv_buf, &recv_len);
+    if (recv_len) {
+        char* read_pos = recv_buf;
+        uint32_t uid = *(uint32_t*)read_pos;
+        read_pos += 32;
+        read_pos += 6;
+        uint8_t type = (*uint8_t*)read_pos;
+        cout << uid << endl;
+        cout << (int)type << endl;
+    }
     return 0;
 }
