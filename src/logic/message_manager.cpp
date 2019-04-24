f4bcedff1d191b18efbad870eab06416e1fa07b4
WhiteCloud0325 <721715576@qq.com>
2019-04-24 10:21:03 +0800

    git add complete message debug

--- src/logic/message_manager.cpp
+++ src/logic/message_manager.cpp
@@ -41,9 +41,13 @@ void MessageManager::ProcessBackwardNoAckMessage(ControlHead *control_head, Conn
 void MessageManager::ProcessCompleteMessage(ControlHead *control_head, Connection_T conn) {
     uint32_t to_id = ntohl(control_head->to_id);
     uint32_t from_id = ntohl(control_head->from_id);
+    if (!database_->InsertStoreMessage(conn, control_head)) {
+        return;
+    }
     if (!database_->InsertOfflineMessage(conn, control_head)) {
         return;
     }
+    
     std::vector<SateParam> sates_to_user;
     database_->GetSateCover(conn, to_id, sates_to_user);
     std::vector<SateParam> sates_from_user;
@@ -59,13 +63,16 @@ void MessageManager::ProcessCompleteMessage(ControlHead *control_head, Connectio
     if (!sates_from_user.empty()) {
         std::string res = ResponseEncode(response);
         SendHelper::GetInstance()->SendMessage(from_id, res, sates_from_user, 10);
-        LOG_DEBUG("Message ProcessCompleteMessage SendReceipt: from_id=%ld", from_id);
+     //   LOG_DEBUG("Message ProcessCompleteMessage SendReceipt: from_id=%ld", from_id);
     }
     if (!sates_to_user.empty()) {
         std::string str = MessageEncode(control_head);
         SendHelper::GetInstance()->SendMessage(to_id, str, sates_to_user, 5);
-        LOG_DEBUG("Message ProcessCompleteMessage SendMessage: from_id=%ld||to_id=%ld||frame_id=%ld", from_id, to_id, ntohs(control_head->frame_id));
+    //    LOG_DEBUG("Message ProcessCompleteMessage SendMessage: from_id=%ld||to_id=%ld||frame_id=%ld", from_id, to_id, ntohs(control_head->frame_id));
     }
+    const boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
+    const boost::posix_time::time_duration td = now.time_of_day();
+    LOG_DEBUG("message from_id=%ld||to_id=%ld||frame_id=%d||time=%lldms", from_id,to_id, ntohs(control_head->frame_id), td.total_milliseconds());
     return;
 }
 
@@ -82,7 +89,6 @@ void MessageManager::ProcessReceipt(ControlHead *control_head, Connection_T conn
     if(database_->GetOfflineMessage(conn, from_id, to_id, frame_id, msg, recv_time) && msg.type == 0x60) {
         database_->DeleteOfflineMessage(conn, from_id, to_id, frame_id);
         LOG_DEBUG("Message Receipt: from_id=%ld||to_id=%ld||frame_id=%ld", from_id, to_id, frame_id);
-        database_->InsertStoreMessage(conn, msg, recv_time);
         /* MessageResponse response;
         response.to_id = htonl(from_id);
         response.from_id = 0;
