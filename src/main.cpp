2df536145eaedab8c343378f393e06e6e79c0908
WhiteCloud0325 <721715576@qq.com>
2019-04-27 12:03:06 +0800

    fix:tcp bug and conf thread num

--- src/main.cpp
+++ src/main.cpp
@@ -49,49 +49,12 @@ int main() {
         return -1;
     }
     boost::thread run_thread(boost::bind(&Controller::Run, &controller)); 
-    boost::thread qos_thread(boost::bind(&QosSendDaemon::Start, &(controller.qos_send_daemon_)));
+  //  boost::thread qos_thread(boost::bind(&QosSendDaemon::Start, &(controller.qos_send_daemon_)));
     while(!stopped) {
         usleep(1000000);
     }
     controller.Stop();
     run_thread.join();
-    qos_thread.join();
-    /*
-    uint32_t from_id = 1;
-    uint32_t to_id = 2;
-    uint16_t frame_id = 3;
-    uint8_t  type = 0x40;
-    uint8_t  retain = 0x00;
-    char content[] = "bbbbbb";
-    char buf[1024] = {0};
-    char *pos = buf;
-    *(uint32_t*)pos = htonl(to_id);
-    pos+=4;
-    *(uint32_t*)pos = htonl(from_id);
-    pos += 4;
-    *(uint16_t*)pos = htons(frame_id);
-    pos +=2;
-    *(uint8_t*)pos++ = type;
-    *(uint8_t*)pos++ = retain;
-    strcpy(pos, content);
-    pos += strlen(content) + 1;
-    std::string request(buf, pos - buf);
-    int port = 10000;
-    std::string server_ip = "162.105.85.118";
-    im::AccessMessage message;
-    message.__set_uid(to_id);
-    std::vector<int32_t> beams = {1, 3};
-    message.__set_beam_id(beams);
-    message.__set_content(request);
-    boost::shared_ptr<TSocket> client_socket(new TSocket(server_ip, port));
-    boost::shared_ptr<TTransport> client_transport(new TBufferedTransport(client_socket));
-    boost::shared_ptr<TProtocol> client_protocol(new TBinaryProtocol(client_transport));
-    boost::shared_ptr<im::LogicInterfaceClient> client(new im::LogicInterfaceClient(client_protocol));
-    std::cout << request.size() << std::endl;
-    client_transport->open();
-    client->LogicToAccess(message);
-    //client->AccessToLogic(request);
-    client_transport->close();
-    */
+   // qos_thread.join();
     return 0;
 }
